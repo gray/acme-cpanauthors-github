71d74a2d891ca00fd89e19535863d65fee659d14
gray <gray@cpan.org>
2011-01-02 15:48:53 -0800

    update copyright dates

--- scripts/generate-github-authors.pl
+++ scripts/generate-github-authors.pl
@@ -152,7 +152,7 @@ L<http://search.cpan.org/dist/Acme-CPANAuthors-GitHub/>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2010-2011 gray <gray at cpan.org>, all rights reserved.
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
